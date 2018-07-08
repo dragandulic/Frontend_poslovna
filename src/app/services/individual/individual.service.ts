04265460ec0d070d5d0fbee2b22ab127a7676be8
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-07-08 18:01:50 +0200

    delete individual

--- src/app/services/individual/individual.service.ts
+++ src/app/services/individual/individual.service.ts
@@ -28,4 +28,10 @@ export class IndividualService {
   }
 
 
+  deleteIndividual(id): Observable<any>{
+
+    return this.http.get<any>('http://localhost:8090/individual/deleteindividual/' + id);
+
+  }
+
 }
