b83456f3a27ff79fae3049018cba92d949f51abc
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-07-11 15:25:16 +0200

    delete and edit legal entity

--- src/app/components/legalentity/legalentity.component.ts
+++ src/app/components/legalentity/legalentity.component.ts
@@ -11,6 +11,8 @@ import { LegalEntity } from '../../legalentity';
 })
 export class LegalentityComponent implements OnInit {
 
+  legalentity: LegalentityComponent;
+  legalentityedit: any={};
   bank: Bank;
   legalentitys: LegalEntity[];
   constructor(private legalentityservice: LegalentityService, private loginservice: LoginService) { }
@@ -20,6 +22,46 @@ export class LegalentityComponent implements OnInit {
     this.bank = this.loginservice.getLocalStore();
     this.legalentityservice.getAllLegalofBank(this.bank.id)
     .subscribe(data => {this.legalentitys = data})
+
+  }
+
+
+  deleteLegalEntity(id): void{
+
+    this.legalentityservice.deleteLegaEntity(id).toPromise()
+    .then(data =>{
+      this.legalentitys = this.legalentitys.filter(el => el.id != id);
+
+  })
+
+
+  }
+
+
+
+  editLegalEntity(): void{
+
+    this.bank = this.loginservice.getLocalStore();
+    
+    this.legalentityservice.editlegalEntity(this.legalentityedit)
+    .subscribe(data =>{this.legalentity=data;
+      this.legalentityservice.getAllLegalofBank(this.bank.id)
+      .subscribe(data =>{this.legalentitys = data
+       
+      });
+    
+    });
+
+  }
+
+
+  findlegalentity(idlegal): void{
+
+    this.bank = this.loginservice.getLocalStore();
+
+    this.legalentityservice.findLegalEntity(idlegal,this.bank.id)
+    .subscribe(data =>{this.legalentityedit = data});
+
 
   }
 
